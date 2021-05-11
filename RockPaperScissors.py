9ef4314aba816e739d286462329ad9655219c4b0
KimShun <82489428+KimShun@users.noreply.github.com>
2021-05-11 14:08:37 +0900

    사용자 수를 입력받기.

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -4,5 +4,5 @@ play = ["Rock", "Paper', "Scissors"]
 computer = play[randint(0, 2)]
 print("Computer: {}".format(computer))
 
-player = "Paper"
+player = input("Rock, Paper, Scissors? : ")
 print("Player: {}".format(player))
