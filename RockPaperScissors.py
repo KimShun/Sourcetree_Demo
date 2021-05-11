8aed42827dd181de6b6fd7664498de5bb71e8fb3
KimShun <kimsih08180@gmail.com>
2021-05-11 14:07:29 +0900

    승리의 조건을 생성

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -6,3 +6,11 @@ print("Computer: {}".format(computer))
 
 player = input("Rock, Paper, Scissors? : ")
 print("Player: {}".format(player))
+
+if player == computer:
+	print("Tie!")
+elif player == "Rock":
+	if computer == "Scissors":
+		print("You Win!")
+	else
+		print("You Lose...")
